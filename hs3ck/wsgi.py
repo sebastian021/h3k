dd3ef0b7e630ef1bbe37baf83a113ee32a0d860a
nima asoud <nima.asoudeh@gmail.com>
2024-04-07 13:00:18 +0330

    djangoStartProject

--- hs3ck/wsgi.py
+++ hs3ck/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for ha3ck project.
+WSGI config for hs3ck project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ha3ck.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hs3ck.settings')
 
 application = get_wsgi_application()
