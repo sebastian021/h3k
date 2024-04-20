a26da8ca4d6fa2113d76d05819eb2513e90728cb
nima asoud <nima.asoudeh@gmail.com>
2024-04-20 19:29:39 +0330

    some change

--- users/serializers.py
+++ users/serializers.py
@@ -3,7 +3,6 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
 from .models import User, UserProfile
 
-
 class UserProfileSerializer(serializers.ModelSerializer):
     
     class Meta:
