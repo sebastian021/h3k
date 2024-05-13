9c058e01b91b59e1a41a47f2a1cf0cec885a6ce1
nima asoud <nima.asoudeh@gmail.com>
2024-05-13 14:39:38 +0330

    remove extra url

--- fixtures/urls.py
+++ fixtures/urls.py
@@ -3,7 +3,7 @@ from .views import FixturesAPIView, FixtureStatistics, FixtureEvents
 
 urlpatterns = [
     path('<int:year>/<str:league>/<str:round>/', FixturesAPIView.as_view(), name='fixtures_api'),
-    path('statistics/<int:fixture_id>/', FixtureStatistics.as_view(), name='fixture_statistics'),    path('statistics/<int:fixture_id>/', FixtureStatistics.as_view(), name='fixture_statistics'),
+    path('statistics/<int:fixture_id>/', FixtureStatistics.as_view(), name='fixture_statistics'),
     path('events/<int:fixture_id>/', FixtureEvents.as_view(), name='fixture_statistics'),
 
     
