53fc99f29385ef9f54a2d17478dc15af19c88df3
nima asoud <nima.asoudeh@gmail.com>
2024-05-24 15:00:48 +0330

    remove / from tables/urls

--- tables/urls.py
+++ tables/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import StandingView
 
 urlpatterns = [
-    path('<str:league_name>/', StandingView.as_view()),
-    path('<str:league_name>/<int:season>/', StandingView.as_view()),
+    path('<str:league_name>', StandingView.as_view()),
+    path('<str:league_name>/<int:season>', StandingView.as_view()),
 ]
