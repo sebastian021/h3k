24459e77eb7dad0b8c57b2f5ea48270a316f110a
nima asoud <nima.asoudeh@gmail.com>
2024-05-25 23:53:25 +0330

    add newsSerializer

--- news/serializers.py
+++ news/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import News
-from .models import UploadImage
+from .models import UploadImage, UploadedVideo
 
 class NewsSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,4 +10,10 @@ class NewsSerializer(serializers.ModelSerializer):
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = UploadImage
+        fields = '__all__'
+
+
+class VideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UploadedVideo
         fields = '__all__'
