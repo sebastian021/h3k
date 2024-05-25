66eff38d135c41d0897bc996aae09316e32fdb25
nima asoud <nima.asoudeh@gmail.com>
2024-05-25 23:50:28 +0330

    add today_date

--- matches/urls.py
+++ matches/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import MatchesAPIView, MatchStatisticsView, MatchEventsView, MatchesLineUpView, MatchesPlayersStatsView
 
 urlpatterns = [
+    path('', MatchesAPIView.as_view(), name='today_matches_api'),
     path('<str:date>', MatchesAPIView.as_view(), name='matches_api'),
     path('statistics/<int:fixture_id>', MatchStatisticsView.as_view(), name='matches_statistics'),
     path('events/<int:fixture_id>', MatchEventsView.as_view(), name='match_events'),
