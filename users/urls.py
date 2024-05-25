8169b28ae2adbf8503be8a463510876ffde8f1dd
nima asoud <nima.asoudeh@gmail.com>
2024-05-25 12:00:33 +0330

    remove / from users

--- users/urls.py
+++ users/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
-    path('register/',RegisterView.as_view(),name="register"),
+    path('register',RegisterView.as_view(),name="register"),
     # path('loogin/', LoginAPIView.as_view(),name="loogin"),
-    path('login/', CustomTokenObtainPairView.as_view(),name="login"),
-    path('logout/', LogoutAPIView.as_view(), name="logout"),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login', CustomTokenObtainPairView.as_view(),name="login"),
+    path('logout', LogoutAPIView.as_view(), name="logout"),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 ]
